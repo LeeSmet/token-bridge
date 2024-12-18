158d17549d0c1f6988003d2bacfd0524c6ac5e03
Lee Smet <lee.smet@hotmail.com>
2024-12-18 16:14:44 +0100

    First commit
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

---
+++ programs/token-bridge/src/lib.rs
@@ -0,0 +1,185 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{
+        create_metadata_accounts_v3, mpl_token_metadata::types::DataV2, CreateMetadataAccountsV3,
+        Metadata,
+    },
+    token::{Mint, TokenAccount},
+    token_2022::{burn, mint_to, Burn, MintTo, Token2022},
+};
+
+declare_id!("Cb1LfTgJkeE6yk62yAY682C12netoiCH3cXEnuEFyUGm");
+
+#[program]
+pub mod token_bridge {
+    use super::*;
+
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        token_name: String,
+        token_symbol: String,
+        token_uri: String,
+    ) -> Result<()> {
+        msg!("Creating metadata account");
+
+        // PDA signer seeds
+        let signer_seeds: &[&[&[u8]]] = &[&[b"mint", &[ctx.bumps.mint_account]]];
+
+        // Cross Program Invocation (CPI) signed by PDA
+        // Invoking the create_metadata_account_v3 instruction on the token metadata program
+        create_metadata_accounts_v3(
+            CpiContext::new(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    metadata: ctx.accounts.metadata_account.to_account_info(),
+                    mint: ctx.accounts.mint_account.to_account_info(),
+                    mint_authority: ctx.accounts.mint_account.to_account_info(), // PDA is mint authority
+                    update_authority: ctx.accounts.mint_account.to_account_info(), // PDA is update authority
+                    payer: ctx.accounts.payer.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+            )
+            .with_signer(signer_seeds),
+            DataV2 {
+                name: token_name,
+                symbol: token_symbol,
+                uri: token_uri,
+                seller_fee_basis_points: 0,
+                creators: None,
+                collection: None,
+                uses: None,
+            },
+            true, // Is mutable
+            true, // Update authority is signer
+            None, // Collection details
+        )?;
+
+        msg!("Token created successfully.");
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    // Create mint account
+    // Same PDA as address of the account and mint/freeze authority
+    #[account(
+        init,
+        seeds = [b"mint"],
+        bump,
+        payer = payer,
+        mint::decimals = 9,
+        mint::authority = mint_account.key(),
+        mint::freeze_authority = mint_account.key(),
+
+    )]
+    pub mint_account: Account<'info, Mint>,
+
+    /// CHECK: Validate address by deriving pda
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), mint_account.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    pub metadata_account: UncheckedAccount<'info>,
+
+    pub token_program: Program<'info, Token2022>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct MintToken<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    // Mint account address is a PDA
+    #[account(
+        mut,
+        seeds = [b"mint"],
+        bump
+    )]
+    pub mint_account: Account<'info, Mint>,
+
+    // Create Associated Token Account, if needed
+    // This is the account that will hold the minted tokens
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint_account,
+        associated_token::authority = payer,
+    )]
+    pub associated_token_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token2022>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
+    msg!("Minting token to associated token account...");
+    msg!("Mint: {}", &ctx.accounts.mint_account.key());
+    msg!(
+        "Token Address: {}",
+        &ctx.accounts.associated_token_account.key()
+    );
+
+    // PDA signer seeds
+    let signer_seeds: &[&[&[u8]]] = &[&[b"mint", &[ctx.bumps.mint_account]]];
+
+    // Invoke the mint_to instruction on the token program
+    mint_to(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            MintTo {
+                mint: ctx.accounts.mint_account.to_account_info(),
+                to: ctx.accounts.associated_token_account.to_account_info(),
+                authority: ctx.accounts.mint_account.to_account_info(), // PDA mint authority, required as signer
+            },
+        )
+        .with_signer(signer_seeds), // using PDA to sign
+        // TODO
+        amount * 10u64.pow(ctx.accounts.mint_account.decimals as u32), // Mint tokens, adjust for decimals
+    )?;
+
+    msg!("Token minted successfully.");
+
+    Ok(())
+}
+
+pub fn burn_token(
+    ctx: Context<MintToken>,
+    amount: u64,
+    chain_id: u64,
+    target_address: String,
+) -> Result<()> {
+    // PDA signer seeds
+    let signer_seeds: &[&[&[u8]]] = &[&[b"mint", &[ctx.bumps.mint_account]]];
+
+    // Invoke the burn instruction on the token program
+    burn(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            Burn {
+                from: ctx.accounts.payer.to_account_info(),
+                mint: ctx.accounts.mint_account.to_account_info(),
+                authority: ctx.accounts.mint_account.to_account_info(), // PDA mint authority, required as signer
+            },
+        )
+        .with_signer(signer_seeds), // using PDA to sign
+        // TODO
+        amount * 10u64.pow(ctx.accounts.mint_account.decimals as u32), // Mint tokens, adjust for decimals
+    )?;
+
+    msg!("Token minted successfully.");
+
+    Ok(())
+}
